ba289bfe9a7082aca06819b85174a04fae10d7c2
Adrián Antón <adrtler@gmail.com>
2022-03-02 12:30:35 +0100

    Cambiando el nombre de la base de datos y agregando la fecha createAt

--- src/main/java/com/bolsaideas/springboot/webflux/app/SpringBootWebfluxApplication.java
+++ src/main/java/com/bolsaideas/springboot/webflux/app/SpringBootWebfluxApplication.java
@@ -1,5 +1,7 @@
 package com.bolsaideas.springboot.webflux.app;
 
+import java.util.Date;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,9 +37,10 @@ public class SpringBootWebfluxApplication implements CommandLineRunner {
 				new Producto("Apple iPod", 46.89), new Producto("Sony Notebook", 846.89),
 				new Producto("Hewlett Packard Multifuncional", 200.89), new Producto("Bianchi Bicicleta", 70.89),
 				new Producto("HP Notebook Omen 17", 2500.89), new Producto("Mica Cómoda 5 Cajones", 150.89),
-				new Producto("TV Sony Bravia OLED 4K Ultra HD", 2255.89))
-				.flatMap(producto -> productoDao.save(producto))
-				.subscribe(producto -> log.info("Insert: " + producto.getId() + " " + producto.getNombre()));
+				new Producto("TV Sony Bravia OLED 4K Ultra HD", 2255.89)).flatMap(producto -> {
+					producto.setCreateAt(new Date());
+					return productoDao.save(producto);
+				}).subscribe(producto -> log.info("Insert: " + producto.getId() + " " + producto.getNombre()));
 	}
 
 }
